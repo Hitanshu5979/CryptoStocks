506afcbaf42ef28c0d54349523347fba61391b9d
"Hitanshu5979" <"hitanshusamantaray@gmail.com">
2022-03-20 18:06:38 +0530

    adding contracts to test branch

---
+++ contracts/company.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+contract Company {
+    uint companyId;
+    uint noOfStocks;
+    uint stockPrice;
+    address[] public companyFiatWallet;
+}
